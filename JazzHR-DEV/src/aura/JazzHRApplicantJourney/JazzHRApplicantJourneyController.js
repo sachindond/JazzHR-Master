1dcc5fc99a6d0898e63b216c58e28dd8d013a90a
Sachin Dond <er.sachindond@gmail.com>
2019-06-02 16:15:56 +0530

    Master Code Commit.

---
+++ JazzHR-DEV/src/aura/JazzHRApplicantJourney/JazzHRApplicantJourneyController.js
@@ -0,0 +1,21 @@
+({
+    doInit:function(component, event, helper) {
+        var obj = component.get("v.selectedLookUpRecord");
+        console.log("***doInit",obj.Id);
+        
+        
+    },
+    handleComponentEvent:function(component, event, helper) {
+		helper.getSelectedApplicant(component, event, helper);
+        
+    },
+    expandJobApplicantionSection : function(component, event, helper) {
+        helper.expandSection(component,event,'expanJobApplicationId');
+    },
+    expandQuestionSection: function(component, event, helper) {
+        helper.expandSection(component,event,'expandQuestionSectionId');
+    },
+    expandEnquirySection: function(component, event, helper) {
+        helper.expandSection(component,event,'expandEnquirySectionId');
+    }
+})
